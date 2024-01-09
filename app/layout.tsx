7ea1d4a4dffc19f4d7da1b486c66b8b8c2e64206
sheninthjr <sheninthjr23@gmail.com>
2024-01-09 11:10:54 +0530

    Recoil name fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import RecoidContextProvider from "./lib/recoilContextProvider";
+import RecoilContextProvider from "./lib/recoilContextProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <RecoidContextProvider>{children}</RecoidContextProvider>
+        <RecoilContextProvider>{children}</RecoilContextProvider>
       </body>
     </html>
   );
